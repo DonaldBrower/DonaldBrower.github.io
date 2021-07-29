a54be3f8891bc6a570accfd9da24c25d36758543
Donald Brower <donald.brower1@gmail.com>
2021-07-29 02:10:40 -0700

    Have the script watch for changes to the template files and apply them to all the existing posts

--- scripts/convertMd.js
+++ scripts/convertMd.js
@@ -9,55 +9,97 @@ const { parse } = require("node-html-parser");
 startWatcher(process.env.MDPATH, process.env.HTMLPATH);
 
 async function startWatcher(mdDir, htmlDir) {
-  chokidar.watch([mdDir]).on("all", async (e, mdFile) => {
-    if (mdFile.match(/\.md/g)) {
-      try {
-        console.log(`.. Converting ${path.basename(mdFile)}`);
-
-        const htmlFile = path.join(
-          htmlDir,
-          path.basename(mdFile).replace(".md", ".html")
-        );
-
-        await convert(mdFile, htmlFile);
-      } catch (err) {
-        throw err;
+  chokidar
+    .watch([mdDir, process.env.TEMPLATEPATH])
+    .on("all", async (e, file) => {
+      if (file.match(/\.md/g)) {
+        await markdownConversion(file, mdDir, htmlDir);
+      } else if (file.match(/\.html/g)) {
+        await templateUpdate(file);
       }
-    }
-  });
+    });
 }
 
-const classMap = {
-  code: "language-js",
-};
+async function templateUpdate() {
+  const promises = [];
 
-const bindings = Object.keys(classMap).map((key) => ({
-  type: "output",
-  regex: new RegExp(`<${key}(.*)>`, "g"),
-  replace: `<${key} class="${classMap[key]}" $1>`,
-}));
+  try {
+    const filenames = await fs.readdir(process.env.HTMLPATH);
 
-bindings.push(function removePFromImg() {
-  return [
-    {
-      type: "output",
-      filter: function (text) {
-        text = text.replace(
-          /(<\/?p[^>]*>)(?=<img.+>)|(<\/?p[^>]*>)(?<=<img.+>)/g,
-          ""
-        );
+    filenames.forEach((file) => {
+      if (file.match(/\.html/g)) {
+        promises.push(async function () {
+          try {
+            await fs.unlink(path.join(process.env.HTMLPATH, file));
 
-        return text;
-      },
-    },
-  ];
-});
+            const markdownFile = path.join(
+              process.env.MDPATH,
+              file.replace(".html", ".md")
+            );
 
-const converter = new showdown.Converter({
-  extensions: [...bindings],
-});
+            const htmlFile = path.join(process.env.HTMLPATH, file);
+
+            await convert(markdownFile, htmlFile);
+          } catch (e) {
+            throw e;
+          }
+        });
+      }
+    });
+
+    await Promise.all(promises.map((fn) => fn()));
+  } catch (e) {
+    throw e;
+  }
+}
+
+async function markdownConversion(mdFile, mdDir, htmlDir) {
+  try {
+    console.log(`.. Converting ${path.basename(mdFile)}`);
+
+    const htmlFile = path.join(
+      htmlDir,
+      path.basename(mdFile).replace(".md", ".html")
+    );
+
+    await convert(mdFile, htmlFile);
+  } catch (err) {
+    throw err;
+  }
+}
 
 async function convert(mdFile, htmlFile) {
+  const classMap = {
+    code: "language-js",
+  };
+
+  const bindings = Object.keys(classMap).map((key) => ({
+    type: "output",
+    regex: new RegExp(`<${key}(.*)>`, "g"),
+    replace: `<${key} class="${classMap[key]}" $1>`,
+  }));
+
+  bindings.push(function removePFromImg() {
+    return [
+      {
+        type: "output",
+        filter: function (text) {
+          text = text.replace(
+            /(<\/?p[^>]*>)(?=<img.+>)|(<\/?p[^>]*>)(?<=<img.+>)/g,
+            ""
+          );
+
+          return text;
+        },
+      },
+    ];
+  });
+
+  const converter = new showdown.Converter({
+    extensions: [...bindings],
+  });
+
+  //********************************************* */
   const md = await getFile(path.join(mdFile));
 
   let html = await getFile(htmlFile);
@@ -71,6 +113,7 @@ async function convert(mdFile, htmlFile) {
   const container = dom.querySelector(".text");
   container.innerHTML = converter.makeHtml(md);
 
+  //error handling on changing files?
   await updateFile(htmlFile, dom.innerHTML);
 }
 
@@ -86,6 +129,7 @@ async function getFile(filePath) {
 
 async function updateFile(filePath, content) {
   try {
+    // it's okay to not do anything if it errors out on unlink. that just means the file isn't there
     try {
       await fs.unlink(filePath);
     } catch (e) {
